7d081c903e4139bbc56a2e7acc4a9c7dbfcd5f02
AntonioAlcM <totidos@hotmail.com>
2017-10-12 11:42:40 +0200

    Añadido test para comprobar que la fecha de los hitos sea anterior a la actual #1

--- test_HitosIV.py
+++ test_HitosIV.py
@@ -1,4 +1,8 @@
+# -*- coding: utf-8 -*-
 import unittest
+import time
+from datetime import datetime
+
 from HitosIV import HitosIV
 
 
@@ -6,14 +10,14 @@ class TestHitosIV(unittest.TestCase):
 
     def setUp(self):
         self.hitos = HitosIV()
-    
+
     def test_should_initialize_object_OK(self):
         self.assertIsInstance(self.hitos,HitosIV, "Objeto creado correctamente")
 
     def test_should_have_hitos_stored_correctly( self):
         self.assertIsInstance( self.hitos.todos_hitos(), dict, "El objeto hitos contiene un diccionario")
         self.assertEqual(self.hitos.cuantos(), 2, "El número de hitos es incorrecto")
-            
+
     def test_should_return_hitos_correctly_and_raise_error(self):
         self.assertEqual( self.hitos.uno(0)["file"],  "0.Repositorio" )
         with self.assertRaises(IndexError):
@@ -21,7 +25,10 @@ class TestHitosIV(unittest.TestCase):
 
         with self.assertRaises(IndexError):
             zipi = self.hitos.uno(99)
+    def test_fecha_hito_anterior_a_ahora(self):
+        fecha_hito = datetime.strptime(str(self.hitos.uno(0)["fecha"]), "%d/%m/%Y")
+        fecha_actual=datetime.now()
+        self.assertLessEqual( fecha_hito, fecha_actual)
 
-     
 if __name__ == '__main__':
     unittest.main()
