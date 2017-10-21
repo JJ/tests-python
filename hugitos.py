d6cab6af25ea62ead62535b6a390387597631a76
JJ Merelo <jjmerelo@gmail.com>
2017-10-21 19:48:40 +0200

    Añade mini-servidor basado en hug

---
+++ hugitos.py
@@ -0,0 +1,13 @@
+"""Hitos de IV servidos para usted"""
+
+import hug
+
+from HitosIV import HitosIV
+
+
+estos_hitos = HitosIV()
+
+@hug.get('/all')
+def all():
+    """Devuelve todos los hitos"""
+    return { "hitos": estos_hitos.todos_hitos() }
