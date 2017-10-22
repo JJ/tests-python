2bfae9e1749716b4002dd8a9744706e3967b05cf
JJ Merelo <jjmerelo@gmail.com>
2017-10-22 11:44:33 +0200

    Añade test para una de las rutas

--- hugitos_test.py
+++ hugitos_test.py
@@ -2,8 +2,12 @@ import hug
 import hugitos
 
 
-data = hug.test.get(hugitos, '/all')
-
 def test_should_have_correct_API():
+    data = hug.test.get(hugitos, '/all')
     assert data.status == "200 OK"
     assert data.data['hitos']['hitos'][0]['file'] == "0.Repositorio"
+
+def test_should_return_at_least_one_element():
+    data = hug.test.get(hugitos, '/one/0')
+    assert data.status == "200 OK"
+    assert data.data['hito']['file'] == "0.Repositorio"
