2965dc9398d55ec3b1e2c51b18a2d459b3d824e9
JJ Merelo <jjmerelo@gmail.com>
2017-11-19 10:19:12 +0100

    Añade logeador y pasan los tests

--- hugitos.py
+++ hugitos.py
@@ -1,13 +1,33 @@
 """Hitos de IV servidos para usted"""
 
 import os
+import logging
+
 import hug
+from hug.middleware import LogMiddleware
+
+from pythonjsonlogger import jsonlogger
 
 from HitosIV import HitosIV
 
+class Logger(object):
+    def __init__(self):
+        self.logger = logging.getLogger()
+        logHandler = logging.StreamHandler()
+        formatter = jsonlogger.JsonFormatter()
+        logHandler.setFormatter(formatter)
+        self.logger.addHandler(logHandler)
+        
+    def info(self, content):
+        self.logger.info(content)
 
 estos_hitos = HitosIV()
 
+@hug.middleware_class()
+class CustomLogger(LogMiddleware):
+    def __init__(self, logger=Logger()):
+        super().__init__(logger=logger)
+        
 @hug.get('/')
 def status():
     """Devuelve estado"""
