30299221219021368a9ec61bd897ecabb2774fbc
JJ Merelo <jjmerelo@gmail.com>
2020-11-03 09:06:18 +0100

    Setting up var initialization

--- HitosIV/etcd-vars.py
+++ HitosIV/etcd-vars.py
@@ -1,12 +1,24 @@
 import etcd3
 from dotenv import load_dotenv
+import os
+import sys
 
-def main():
+def main( argv = [] ):
+    PORT_VAR_NAME= 'hugitos:PORT'
     etcd = etcd3.client()
-    load_dotenv()
-    etcd.put('PORT',PORT)
+    if ( argv ):
+        etcd.put(PORT_VAR_NAME,argv[0])
+    else:
+        load_dotenv()
+        if (os.getenv('PORT')):
+            etcd.put(PORT_VAR_NAME,os.getenv('PORT'))
+        else:
+            etcd.put(PORT_VAR_NAME,3000)
 
 
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) > 1:
+        main(sys.argv[1:])
+    else:
+        main()
