395c8e15bc44264414d41b842f6c76baff5473f7
JJ Merelo <jjmerelo@gmail.com>
2020-12-21 13:42:46 +0100

    Converts to class

--- HitosIV/starlitos.py
+++ HitosIV/starlitos.py
@@ -12,7 +12,15 @@ async def hitos(request):
     """Devuelve todos los hitos"""
     return JSONResponse( estos_hitos.todos_hitos() )
 
+async def creaHito( request ):
+    """ Crea un hito """
+    file = request.path_params["file"]
+    data = await request.json()
+    data['file'] = file
+    estos_hitos['hitos'].append(data)
+
 
 rutas = Router( routes = [
-    Route("/hitos", endpoint=hitos, methods=["GET"])
+    Route("/hitos", endpoint=hitos, methods=["GET"]),
+    Route("/hitos/{file}", endpoint=creaHito, methods=["PUT"] )
 ])
--- starlitos_test.py
+++ starlitos_test.py
@@ -1,12 +1,19 @@
+from unittest import TestCase
 from starlette.responses import JSONResponse
 from starlette.testclient import TestClient
 from HitosIV.starlitos import rutas
 
-def test_should_have_correct_API():
-    client = TestClient(rutas)
-    response = client.get('/hitos')
-    assert response.status_code == 200
-    data = response.json()
-    print(data)
-    for i in ['hitos','comment']:
-        assert data[i]
+class StarletteTesting(TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super(StarletteTesting, self).__init__(*args, **kwargs)
+        self.client = TestClient(rutas)
+
+    def test_should_have_correct_API( self ):
+        client = self.client
+        response = client.get('/hitos')
+        assert response.status_code == 200
+        data = response.json()
+        print(data)
+        for i in ['hitos','comment']:
+            assert data[i]
